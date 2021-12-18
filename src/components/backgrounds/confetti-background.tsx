23c10a25d3997a217ab02ad38d0858d2e989c19f
Aaron McAdam <aaron@aaronmcadam.com>
2021-12-18 15:36:01 +0000

    fix: make the confetti bg slightly more transparent

--- src/components/backgrounds/confetti-background.tsx
+++ src/components/backgrounds/confetti-background.tsx
@@ -4,7 +4,7 @@ import { ConfettiSlice } from "./confetti-slice";
 export function ConfettiBackground({}) {
   return (
     <Box w="full" pos="relative">
-      <Box w="full" pos="absolute" zIndex="hide" opacity={0.7}>
+      <Box w="full" pos="absolute" zIndex="hide" opacity={0.6}>
         <ConfettiSlice number={1} />
         <ConfettiSlice number={2} />
         <ConfettiSlice number={3} />
